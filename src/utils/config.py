cc061b01d64a32adc69345b45712da8c9725d5ba
Pau Agustí Fernandez <pauagustifernandez3@gmail.com>
2025-08-21 16:42:33 +0200

    Added more configs and env example

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,4 +1,8 @@
 import os
 
-OLLAMA_MODEL = os.getenv("OLLAMA_MODEL", "llama3")
-EMBEDDINGS_BACKEND = os.getenv("EMBEDDINGS_BACKEND", "ollama")
+OLLAMA_MODEL = os.getenv("OLLAMA_MODEL")
+EMBEDDINGS_BACKEND = os.getenv("EMBEDDINGS_BACKEND")
+EMBEDDINGS_MODEL = os.getenv("EMBEDDINGS_MODEL")
+TOP_K = int(os.getenv("TOP_K"))
+CHUNK_SIZE = int(os.getenv("CHUNK_SIZE"))
+CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP"))
