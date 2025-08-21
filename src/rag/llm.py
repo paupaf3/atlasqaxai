71254a5af1125c423098a7ca95dbf194bd9ec80d
Pau Agustí Fernandez <pauagustifernandez3@gmail.com>
2025-08-21 16:23:25 +0200

    Added comment with the link to more info about the params, to make the model more conservative

--- src/rag/llm.py
+++ src/rag/llm.py
@@ -7,4 +7,8 @@ def get_llm(model: str = "llama3"):
     # But a few things can still introduce variation
 
     # So temperature=0 makes the model """"""""deterministic""""""""", hence the """"""""""
+
+    # There also some other important params like top_k or top_p, to adjust answers to more conservative or more creative and diverse ways
+    # Read more at:
+    # https://python.langchain.com/api_reference/ollama/chat_models/langchain_ollama.chat_models.ChatOllama.html#langchain_ollama.chat_models.ChatOllama.temperature
     return ChatOllama(model=model, temperature=0)
